6b12410d7478931cf47ec23a1aa3a6e1997be333
Leonel <mendiolaleonel2002@gmail.com>
2026-06-17 19:34:24 -0600

    Pureba #2 de lint

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -5,23 +5,29 @@ from generator.password_generator import (
     has_symbol
 )
 
+
 def test_password_length():
     password = generator_password(12)
     assert len(password) == 12
+
 
 def test_password_is_string():
     password = generator_password(10)
     assert isinstance(password, str)
 
+
 def test_has_uppercase():
     assert has_uppercase("Abc") is True
+
 
 def test_has_number():
     assert has_number("abc1") is True
 
+
 def test_has_symbol():
-    assert has_symbol("abc!") is True 
-    
+    assert has_symbol("abc!") is True
+
+
 def test_generator_password_length_range():
     for length in range(4, 20):
         password = generator_password(length)
